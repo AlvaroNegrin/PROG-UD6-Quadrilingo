c5b3823ed99edadcf212651c784c5584a3e89d48
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2024-05-09 13:39:22 +0200

    feat(Dictionary):Add delete and show words methods

--- src/net/quadrilingo/dictionaries/Dictionary.java
+++ src/net/quadrilingo/dictionaries/Dictionary.java
@@ -19,12 +19,31 @@ public class Dictionary {
     }
 
     public void addWord(String word) {  
-        String wordFormatted = formatText(word);
+        String wordFormatted = this.formatText(word);
         String wordFirstLetter = Character.toString(wordFormatted.charAt(0));
         if (dictionary.containsKey(wordFirstLetter)) {
             dictionary.get(wordFirstLetter).add(wordFormatted);
         }
     }
+
+    public void deleteWord(String word) {
+        String wordFormatted = this.formatText(word);
+        String wordFirstLetter = Character.toString(wordFormatted.charAt(0));
+        if (dictionary.containsKey(wordFirstLetter)) {
+            dictionary.get(wordFirstLetter).remove(wordFormatted);
+        }
+    }
+
+    public String showWordIfExists(String word) {
+        String wordFormatted = formatText(word);
+        String wordFirstLetter = Character.toString(wordFormatted.charAt(0));
+        return dictionary.get(wordFirstLetter).contains(wordFormatted) ? "¡Se ha encontrado la palabra " + wordFormatted + " en el diccionario!" : 
+        "La palabra " + wordFormatted + " no esta en el diccionario.";
+        
+    }
+
+
+
     private String formatText(String text) {
         return text.toLowerCase().replaceAll("\\s", "");
     }
