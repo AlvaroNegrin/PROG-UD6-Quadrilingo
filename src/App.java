3ae2897a8d991f0172d64f0bdaa3ea547006b6a9
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2024-05-10 08:55:08 +0200

    feat(App):Add main loop with main program on it

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import net.quadrilingo.menu.Menu;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Menu.setKeepAsking(true);
+        while (Menu.isKeepAsking()) {
+            Menu.showMainMenu();
+            Menu.optionMenu();
+        }
     }
 }
